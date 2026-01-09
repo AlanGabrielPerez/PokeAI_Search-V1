dc1074e437748057635f8f41e0a5e5101743cc96
AlanGabrielPerez <139288533+AlanGabrielPerez@users.noreply.github.com>
2026-01-09 12:58:10 -0300

    Agregar esquemas Pydantic para la respuesta de búsqueda de Pokémon

---
+++ Backend/app/schemas/Pokemon.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import List
+
+class PokemonResquese(BaseModel):
+    query: str
+
+class PokemonStat(BaseModel):
+    hp: int
+    attack: int
+    defense: int
+    special_attack: int
+    special_defense: int
+    speed: int
+
+
+class PokemonDetail(BaseModel):
+    
+    name: str
+    img: str
+    type: List[str]
+    abilities: List[str]
+    stats: PokemonStat
+    
+class SearchPokemonResponse(BaseModel):
+    total: int
+    pokemon: List[PokemonDetail]
